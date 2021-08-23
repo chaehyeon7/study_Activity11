e5df61bf65988b9507bba83c7bce30e504c6dd1e
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-23 19:20:07 +0900

    Edit SharedPreferences추가 Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity11/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity11/MainActivity.java
@@ -2,27 +2,37 @@ package kr.hs.emirim.chaehyeon.study_activity11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView value;
     Button btn_up, btn_down;
-    int n = 0
+    int n = 0;
+
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
         value = findViewById(R.id.value);
         btn_up = findViewById(R.id.btn_UP);
         btn_down = findViewById(R.id.btn_down);
 
-        value.setText(String.valueOf());  //String으로 넣어야 한다.
+        value.setText(String.valueOf(n));  //String으로 넣어야 한다.
 
         btn_up.setOnClickListener( click);
         btn_down.setOnClickListener( click );
@@ -42,4 +52,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //n값을 저장
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putInt( "save", n);
+        edit.commit();//n값을 물리적으로 저장
+
+    }
 }
